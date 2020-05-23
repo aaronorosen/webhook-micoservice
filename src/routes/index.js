7779e7fe6a2145a30fcc64416d63f76fa9c85ea1
Aronis Mariano <aox.mariano@gmail.com>
2020-05-22 20:26:49 -0400

    add swagger api docs
    use swaggerUi to display logs in route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,21 @@
 const express = require('express');
 const api = require('./api');
 const middleware = require('../middleware');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc({
+  definition: {
+    openapi: '3.0.0', // Specification (optional, defaults to swagger: '2.0')
+    info: {
+      title: 'Webhook Crud', // Title (required)
+      version: '1.0.0', // Version (required)
+    },
+  },
+  // Path to the API docs
+  apis: ['./src/routes/api.js'],
+});
 
 const router = express.Router();
 
@@ -12,6 +27,8 @@ const router = express.Router();
 // region custom routes
 router.use('/api/', api.router);
 // endregion
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 router.use(middleware.errorHandler);
 router.use(middleware.standardHandler);
